a7e919cb3dbdfab5cf4a1c289d29da9cbcd118ce
Damian <damian.thoma@student.htldornbirn.at>
2025-10-14 10:35:58 +0200

    TS_02_Aufgabe_ArrayFunctions_finished

--- WebBasics/TS/02_Aufgabe_ArrayFunctions/script.ts
+++ WebBasics/TS/02_Aufgabe_ArrayFunctions/script.ts
@@ -20,11 +20,7 @@ function printCarsMap(cars: Car[]): void {
 
 printCarsMap(carsList);
 
-function getTotalPriceReduce(cars: Car[]): number {
-    return cars.reduce((sum:number, car:Car):number => sum + car.price, 0);
-}
 
-console.log("Total Price:", getTotalPriceReduce(carsList));
 
 function getExpensiveCarsFilter(cars: Car[], minPrice: number): Car[] {
     return cars.filter((car:Car) => car.price > minPrice);
@@ -32,14 +28,43 @@ function getExpensiveCarsFilter(cars: Car[], minPrice: number): Car[] {
 
 console.log("Expensive Cars:", getExpensiveCarsFilter(carsList, 45000));
 
+
+
 function sortCarsByYear(cars: Car[]): Car[] {
     return cars.sort((a:Car, b:Car) => a.year - b.year);
 }
 
 console.log("Cars sorted by year:", sortCarsByYear(carsList));
 
-function findCarByModel(cars: Car[], model: string): Car {
+
+
+function findCarByModel(cars: Car[], model: string): Car | undefined {
+    // @ts-ignore
     return cars.find((car:Car) => car.model === model);
 }
 
 console.log("Find Car by Model (Model 3):", findCarByModel(carsList, "Model 3"));
+
+
+
+function getTotalPriceReduce(cars: Car[]): number {
+    return cars.reduce((sum:number, car:Car):number => sum + car.price, 0);
+}
+
+console.log("Total Price:", getTotalPriceReduce(carsList));
+
+
+
+function someCarsAbovePrice(cars: Car[], price: number): boolean {
+    return cars.some((car:Car) => car.price > price);
+}
+
+console.log("Some cars above 50000€:", someCarsAbovePrice(carsList, 50000));
+
+
+
+function everyCarAboveYear(cars: Car[], year: number): boolean {
+    return cars.every((car:Car) => car.year > year);
+}
+
+console.log("Every car above year 2018:", everyCarAboveYear(carsList, 2018));
