c23ca39f75d4f35d02c8fe06d454d0e1c50f505c
Damian <damian.thoma@student.htldornbirn.at>
2025-11-04 12:01:40 +0100

    React_Aufgabe_02_1_useState_finished

--- WebBasics/React/ReactApp/src/components/Button.tsx
+++ WebBasics/React/ReactApp/src/components/Button.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function Button({ text }: Props) {
   return (
-    <button className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 text-center">
+    <button className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 text-center cursor-pointer">
       {text}
     </button>
   )
--- WebBasics/React/ReactApp/src/components/Card.tsx
+++ WebBasics/React/ReactApp/src/components/Card.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+
 type Props = {
     name: string;
     job: string;
 }
 
 export default function Card({ name, job }: Props) {
+    const [amountOfClicks, setAmountOfClicks] = useState(0);
+    let style = amountOfClicks > 5 ? "bg-green-500" : "bg-white";
   return (
-    <div className="bg-white overflow-hidden w-64 border-1">
+    <div className={`${style} overflow-hidden w-64 border-1 cursor-pointer`} onClick={() => setAmountOfClicks(amountOfClicks + 1)}>
       <div className="w-full">
         <img 
           src="/img_avatar.png" 
@@ -16,6 +20,8 @@ export default function Card({ name, job }: Props) {
         <h1 className="text-xl font-bold text-black mb-1">{name}</h1>
         <p className="text-base text-black">{job}</p>
       </div>
+      <div className="p-4">Amount of clicks: {amountOfClicks}</div>
     </div>
   )
 }
+
