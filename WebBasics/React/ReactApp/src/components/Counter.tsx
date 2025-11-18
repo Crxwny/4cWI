ac7d0ef5c2f24c4c59b4e75cca4842043ee61e6d
Damian <damian.thoma@student.htldornbirn.at>
2025-11-18 10:26:31 +0100

    React_Aufgabe_State_01_Counter_finished2

--- WebBasics/React/ReactApp/src/components/Counter.tsx
+++ WebBasics/React/ReactApp/src/components/Counter.tsx
@@ -4,12 +4,12 @@ type Props = {}
 
 export default function Counter({}: Props) {
     const [count, setCount] = useState(0);
-    let style = count > 10 ? "bg-green-500" : "bg-white";
+    let style = count >= 10 ? "bg-green-500" : "bg-white";
     return (
-        <div className={`${style} overflow-hidden w-64 border-1 cursor-pointer flex flex-col items-center justify-center`}>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(0)}>Reset</button>
+        <div className={`${style} overflow-hidden w-64 border-1 flex flex-col items-center justify-center`}>
+            <button className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 m-2 text-center cursor-pointer" onClick={() => setCount(count + 1)}>+</button>
+            <button className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 m-2 text-center cursor-pointer" onClick={() => setCount(count - 1)}>-</button>
+            <button className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 m-2 text-center cursor-pointer" onClick={() => setCount(0)}>Reset</button>
             <div className="p-4">Count: {count}</div>
         </div>
   )
